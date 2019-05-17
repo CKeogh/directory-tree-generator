c061edd0d4dd597b6511b11ac08b07abc8c3d59c
CKeogh <c.keogh.10.ck@gmail.com>
2019-05-17 14:14:52 +0100

    find path with os and print recursive directories

--- treeGenerator.py
+++ treeGenerator.py
@@ -1,5 +1,27 @@
 import argparse
+from os import path, listdir
 
-parser = argparse.ArgumentParser(description='display the directory tree for current working directory')
+parser = argparse.ArgumentParser(
+    description='display the directory tree for current working directory')
 parser.add_argument('path', metavar='path/to/directory', type=str,
                     help='path to the directory to display tree of')
+
+args = parser.parse_args()
+
+
+def createTree(filePath):
+    if (not path.exists(filePath)):
+        return 'Error: Invalid path'
+
+    if (path.isfile(filePath)):
+        return '-' + path.basename(filePath)
+
+    treeString = path.basename(filePath) + '\n'
+    for item in listdir(filePath):
+
+        treeString += '|' + createTree(filePath + '/' + item) + '\n'
+
+    return treeString
+
+
+print(createTree(args.path))
